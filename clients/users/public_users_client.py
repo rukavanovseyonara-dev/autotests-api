66c41f478bfdedd4b3d99e543008b254b819d975
rukavanovseyonara-dev <rukavanov.seyonara@yandex.ru>
2026-07-13 21:04:12 +0500

    Prактика: аписание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,36 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.api_client import APIClient
+
+
+class CreateUserRequestDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с /api/v1/users
+    """
+
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
+        """
+        Метод создает нового пользователя.
+
+        :param request: Словарь с данными пользователя:
+            - email: email пользователя
+            - password: пароль
+            - lastName: фамилия
+            - firstName: имя
+            - middleName: отчество
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post("/api/v1/users", json=request)
